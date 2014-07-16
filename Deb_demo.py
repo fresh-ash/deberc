8b8ea6e3e4c07bf884782668959211a8b56b1e86
fresh-ash <sergei081@gmail.com>
2014-07-16 04:28:16 +0300

    Update Deb_demo.py

--- Deb_demo.py
+++ Deb_demo.py
@@ -15,34 +15,38 @@ class Gamer():
 		self.deberc = []	
 	
 	def m_combination(self, trump):
-		for i in self.m_cards.sort():
-			for j in range(4):
-				if i[0] == 12 and i[1] == trump and (i+1) < self.m_cards.len() and (i+1)[0] == 13 and (i+1)[1] == trump:
-					self.bella = True
+		pass
 				
 
 class Diller():
-	"""Класс диллер, который может генерировать колоду и раздавать карты в различных играх"""
-	cards = []
-	trump = None
-	talon = None
+	"""
+			Класс диллер, который может генерировать колоду и раздавать карты в
+			различных играх
+	"""
+	def __init__(self):
+		self.cards = []
+		self.trump = None
+		self.talon = None
+	
 	def coloda(self, start):
 		"""Генерирует колоду карт для соответствующей игры"""
 		for i in range(start, 15):
 			for j in range(4):
-				self.cards.append([i,j])
-		return (self.cards) 
+				self.cards.append([i,j]) 
 
-	def deberc_dill(self, cards, g1, g2, g3, g4):
+	def dill_card(self, g_cards, number):
+		for i in range(number):
+			g_cards.append(self.cards.pop(self.cards.index(choice(self.cards))))	
+	
+	def deberc_dill(self, g1, g2, g3, g4):
 		"""Раздача карт в "Деберц" """
-		g1.dill = True
-		for i in range(6):
-			g1.m_cards.append(cards.pop(cards.index(choice(cards))))
-			g2.m_cards.append(cards.pop(cards.index(choice(cards))))    #КРИВО, но работает!
-			g3.m_cards.append(cards.pop(cards.index(choice(cards))))    #Запилить все в функцию и вызывать для каждого
-			g4.m_cards.append(cards.pop(cards.index(choice(cards))))    #игрока?!
-		self.talon =  cards.pop(cards.index(choice(cards)))                      
-		self.trump = self.talon[1]                                                              #Карта прикупа и козырь!
+		self.coloda(7)
+		self.dill_card(g1.m_cards, 6)
+		self.dill_card(g2.m_cards, 6)
+		self.dill_card(g3.m_cards, 6)
+		self.dill_card(g4.m_cards, 6)
+		self.talon = self.cards.pop(self.cards.index(choice(self.cards)))
+		self.trump = self.talon[1]
 		
 	def deberc_combination(self, gamer):
 		pass
@@ -54,7 +58,6 @@ c = Gamer()
 d = Gamer()
 
 dill = Diller()
-dill.deberc_dill(dill.coloda(7), a, b, c, d)
-
-print (a)
+dill.deberc_dill(a,b,c,d)
+print (a.m_cards)
 
