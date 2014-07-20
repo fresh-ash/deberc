f6d5405514f43caf87658f598d1a51f4e762e493
fresh-ash <sergei081@gmail.com>
2014-07-20 16:09:49 +0300

    Update Deb_demo.py

--- Deb_demo.py
+++ Deb_demo.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-from itertools import product
 from random import choice
 
 class Gamer():
@@ -13,10 +12,10 @@ class Gamer():
 		self.terc = []
 		self.poltos = []
 		self.deberc = []	
-	
+
 	def m_combination(self, trump):
 		pass
-				
+
 
 class Diller():
 	"""
@@ -27,7 +26,7 @@ class Diller():
 		self.cards = []
 		self.trump = None
 		self.talon = None
-	
+
 	def coloda(self, start):
 		"""Генерирует колоду карт для соответствующей игры"""
 		for i in range(start, 15):
@@ -37,21 +36,27 @@ class Diller():
 	def dill_card(self, g_cards, number):
 		for i in range(number):
 			g_cards.append(self.cards.pop(self.cards.index(choice(self.cards))))	
-	
-	def deberc_dill(self, g1, g2, g3, g4):
+
+	def deberc_dill(self, g1, g2, g3, g4, dop_dill = None):
 		"""Раздача карт в "Деберц" """
-		self.coloda(7)
-		self.dill_card(g1.m_cards, 6)
-		self.dill_card(g2.m_cards, 6)
-		self.dill_card(g3.m_cards, 6)
-		self.dill_card(g4.m_cards, 6)
-		self.talon = self.cards.pop(self.cards.index(choice(self.cards)))
-		self.trump = self.talon[1]
-		
+		if dop_dill == None:
+			self.coloda(7)
+			self.dill_card(g1.m_cards, 6)
+			self.dill_card(g2.m_cards, 6)
+			self.dill_card(g3.m_cards, 6)
+			self.dill_card(g4.m_cards, 6)
+			self.talon = self.cards.pop(self.cards.index(choice(self.cards)))
+			self.trump = self.talon[1]
+		else:
+			self.dill_card(g1.m_cards, 2)
+			self.dill_card(g2.m_cards, 2)
+			self.dill_card(g3.m_cards, 2)
+			#self.dill_card(g4.m_cards, 2)
+
 	def deberc_combination(self, gamer):
 		pass
-	
-			
+
+
 a = Gamer()
 b = Gamer()		
 c = Gamer()
@@ -59,5 +64,5 @@ d = Gamer()
 
 dill = Diller()
 dill.deberc_dill(a,b,c,d)
+dill.deberc_dill(a,b,c,d, dop_dill = 1)
 print (a.m_cards)
-
